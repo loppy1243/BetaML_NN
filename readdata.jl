27d7736796e38b35259225bb47359fec278d56f2
Nicholas Todoroff <loppy1243@gmail.com>
2018-06-26 13:19:27 -0400

    Add readdata.jl

---
+++ readdata.jl
@@ -0,0 +1,42 @@
+using JuliennedArrays: julienne
+using Base.Iterators: drop, take
+using IterTools: takenth, chain
+using Lazy: @>
+
+const EVENTS = 10^6
+const COLUMNS = 16*16 + 6
+const ROWS = EVENTS
+const GRIDSIZE = [16, 16]
+const CELLS = prod(GRIDSIZE)
+
+const XYMIN = -[48, 48]/2 # = [-24, -24]
+const XYMAX = [48, 48]/2 # = [24, 24]
+const XYOFF = -[48, 48]/2 # = [-24, -24]
+
+function readdata(file)
+    data = readdlm(file, Float32, dims=(ROWS, COLUMNS))
+
+    (map(x -> sparse(reshape(x, GRIDSIZE...)), julienne(data[:, 1:end-6], (*, :))),
+     julienne(data[:, end-5:end], (*, :)))
+end
+
+function readdata(file, range::Range{T}) where T <: Integer
+    @assert start(range) > 0
+    buf = IOBuffer()
+    open(file) do stream
+        itr = @> stream begin
+            eachline(chomp=false)
+            drop(first(range)-1)
+            i -> chain([first(i)], takenth(drop(i, 1), step(range)))
+            take(length(range))
+        end
+
+        for line in itr; write(buf, line) end
+    end
+
+    seekstart(buf)
+    data = readdlm(buf, Float32, dims=(length(range), COLUMNS))
+
+    (map(x -> sparse(reshape(x, GRIDSIZE...)), julienne(data[:, 1:end-6], (*, :))),
+     julienne(data[:, end-5:end], (*, :)))
+end
