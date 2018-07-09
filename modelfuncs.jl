cd42bbe7ef492332016c6f771ffe518601007862
Nicholas <todoroff@nscl.msu.edu>
2018-07-09 13:50:36 -0400

    Batch prediction computations so that we don't run out of memory

--- modelfuncs.jl
+++ modelfuncs.jl
@@ -1,3 +1,5 @@
+using StatsBase: mode
+
 macro modelfunc(funcdecl)
     @assert funcdecl.head == :function || funcdecl.head == :(=) #=
          =# && funcdecl.args[1].head == :call
@@ -12,13 +14,25 @@ macro modelfunc(funcdecl)
     end
 end
 
-@modelfunc function disthist(model, events, points; model_name="")
-    print("Computing predictions...")
-    preds = squeeze(mapslices(x -> model(x) |> Flux.Tracker.data, events, 2:3), 3)
+@modelfunc function pointhist(model, events, points; model_name="", batchsize=1000)
+    numevents = size(events, 3)
+    print("Computing predictions (out of $numevents)...")
+    preds = Array{Float64}(2, numevents)
+    for i = 0:batchsize:numevents-1
+        print(" ", i)
+        pred_dists, pred_rel_point_dists = model(events[:, :, i+1:min(end, i+batchsize)]) .|> data
+        for j in indices(pred_dists, 3)
+            dist = pred_dists[:, :, j]
+            pred_cell = ind2sub(dist, indmax(dist))
+            pred_cell_point = cellpoint(pred_cell)
+            pred_rel_point = pred_rel_point_dists[pred_cell..., :, j]
+            preds[:, i+j] .= pred_cell_point + pred_rel_point
+        end
+    end
     println(" Done.")
   
     print("Computing distances...")
-    dists = mapslices(norm, preds - points, 2)
+    dists = mapslices(norm, preds - points, 1)
     println(" Done.")
 
     print("Computing statistics...") 
@@ -31,10 +45,6 @@ end
 
     print("Generating histogram...")
     linhist = stephist(dists, legend=false, title=model_name)
-    xmin, xmax = xlims()
-    ymin, ymax = ylims()
-    xrel(f) = xmin + f*(xmax - xmin)
-    yrel(f) = ymin + f*(ymax - ymin)
     annotate!(xrel(0.7), yrel(0.9),
               "Min -- Max: "*string(round(m, 3))*" -- "*string(round(M, 3)))
     annotate!(xrel(0.7), yrel(0.8), "Mean: "*string(round(me, 3)))
