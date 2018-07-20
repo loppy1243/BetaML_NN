3a2676606d0acb8b0e2ec167b6d164025d10426e
Nicholas <todoroff@nscl.msu.edu>
2018-07-20 17:11:36 -0400

    Create good framework for models

--- BetaML_NN2/co.jl
+++ BetaML_NN2/co.jl
@@ -1,18 +1,18 @@
+using Flux
+using Flux.Tracker
+using Lazy: @>
+
+macro :(>>>)(args...)
+    quote
+        x -> @> x $(map(esc, args)...)
+    end
+end
+
 abstract type CO <: ModelType end
 
 const NODES = prod(2(GRIDSIZE.-1).+1)
+const SCALE = (XYMAX - XYMIN)./2GRIDSIZE
 
 regularize(x) = reshape(x/MAX_E, GRIDSIZE, 1, :)
 
-distlayer() = Chain(Conv((3, 3), 1=>1, pad(1, 1), init=ones),
-                    x -> reshape(x, CELLS, :),
-                    softmax,
-                    x -> reshape(x, GRIDSIZE..., :))
-
-function pointlayer(N)
-    denselayer = Chain(Dense(NODES, N), Dense(N, 2))
-
-    (x, cells) -> recenter(x, cells)
-end
-
-const model = Chain(regularize, distlayer(), pointlayer(2CELLS))
+model(activ, N) = Chain(regularize, CellLayer(), @concat(identity..., PointLayer(activ, N))
--- BetaML_NN2/model.jl
+++ BetaML_NN2/model.jl
@@ -5,12 +5,14 @@ abstract type ModelType
 struct Model{T<:ModelType}
     params::Vector{Any}
     hyperparams::Dict{String}
+
+    Model{<:ModelType}(params, hyparams...) = new(params, Dict(hyparams))
 end
 
-predict(m::Model) = (args...,) -> m(args...)
+predict(m::Model) = (args...,) -> predict(m, args...)
 function predict(m::Model, args...)
     x = m(args...)
     istracked(x) ? data(x) : map(data, x)
 end
 
-loss(m::Model) = (args...,) -> loss(args...)
+loss(m::Model) = (args...,) -> loss(m, args...)
